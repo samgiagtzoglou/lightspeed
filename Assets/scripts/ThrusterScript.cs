a0afd0e3c5a4643ee03198fcb0f337c8dafadf3a
cjustice <connorjustice@gmail.com>
2016-02-28 16:30:43 -0500

    commented

--- Assets/scripts/ThrusterScript.cs
+++ Assets/scripts/ThrusterScript.cs
@@ -19,9 +19,13 @@ public class ThrusterScript : MonoBehaviour {
 			float distancePercentage;
 
 			if (Physics.Raycast (thruster.position, thruster.up * -1, out hit, thrusterDistance)) {
+				//The thruster is within thrusterdistance to the ground. How far away?
 				distancePercentage = 1 - (hit.distance / thrusterDistance);
+				//Calculate how much force to push
 				downwardForce = transform.up * thrusterStrength * distancePercentage;
+				//Correct the force for mass of car and deltaTime
 				downwardForce = downwardForce * Time.deltaTime * rb.mass;
+				//Apply the force where the thrusters are
 				rb.AddForceAtPosition (downwardForce, thruster.position);
 			}
 		}
--- Assets/scripts/TrackObject.cs
+++ Assets/scripts/TrackObject.cs
@@ -8,13 +8,16 @@ public class TrackObject : MonoBehaviour {
 	public float distanceBack;
 	public float minimumHeight;
 
+	//Not used directly
 	private Vector3 positionVelocity;
 
 	void FixedUpdate() {
+		//Calculate a new position to place the camera
 		Vector3 newPosition = target.position + (target.forward * distanceBack);
 		newPosition.y = Mathf.Max (newPosition.y + distanceUp, minimumHeight);
-
+		//Move the camera
 		transform.position = Vector3.SmoothDamp (transform.position, newPosition, ref positionVelocity, 0.18f);
+		//Rotate to look at where car is pointing
 		Vector3 focalPoint = target.position + (target.forward * 5); 
 		transform.LookAt (focalPoint);
 	}
