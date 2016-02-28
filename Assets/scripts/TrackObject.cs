f7951284a005a56299443d74d26d3393d6a17485
cjustice <connorjustice@gmail.com>
2016-02-28 15:58:01 -0500

    added basic hovercar

--- Assets/scripts/TrackObject.cs
+++ Assets/scripts/TrackObject.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class TrackObject : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	public Transform target;
+	public float distanceUp;
+	public float distanceBack;
+	public float minimumHeight;
+
+	private Vector3 positionVelocity;
+
+	void FixedUpdate() {
+		Vector3 newPosition = target.position + (target.forward * distanceBack);
+		newPosition.y = Mathf.Max (newPosition.y + distanceUp, minimumHeight);
+
+		transform.position = Vector3.SmoothDamp (transform.position, newPosition, ref positionVelocity, 0.18f);
+		Vector3 focalPoint = target.position + (target.forward * 5); 
+		transform.LookAt (focalPoint);
 	}
 }
