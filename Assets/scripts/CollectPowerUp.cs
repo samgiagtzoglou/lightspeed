295fb6bfd3fab5d059f692961d04693b13158a89
Sam Giagtzoglou <sgiagtzoglou@wesleyan.edu>
2016-02-28 14:43:49 -0500

    main branch

---
+++ Assets/scripts/CollectPowerUp.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectPowerUp : MonoBehaviour {
+	
+	void OnTriggerEnter(Collider other) {
+		//Debug.Log (this.gameObject.GetComponent<SimpleMovement>().speed);
+		this.GetComponent<SimpleMovement>().speed = 20;
+		this.GetComponent<Rigidbody>().AddForce (0, 10f, 0, ForceMode.Impulse);
+	}
+}
---
+++ Assets/scripts/SimpleMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleMovement : MonoBehaviour {
+	public float speed;
+	public Rigidbody rb;
+	public float rotateSpeed;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	public void setSpeed(float newSpeed) {
+		speed = newSpeed;
+	}
+	// Update is called once per frame
+	void Update () {
+		//transform.Translate (speed * Input.GetAxis ("Horizontal") * Time.deltaTime, 0f, speed * Input.GetAxis ("Vertical") * Time.deltaTime);
+		transform.Rotate(0f, 0f, rotateSpeed * -Input.GetAxis("Horizontal"));
+	}
+
+	void FixedUpdate() {
+		Debug.Log (transform.eulerAngles.y);
+		rb.AddForce (rb.velocity.z * (1-(transform.eulerAngles.y/180f)), 0f, speed * Input.GetAxis ("Vertical"));
+	}
+}
