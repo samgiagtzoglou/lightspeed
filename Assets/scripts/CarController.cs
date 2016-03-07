4f4627e3b34cb003df124f5e5093c9509ae50d55
grantaddis <grant@addis.net>
2016-03-07 14:07:12 -0500

    removed loss of control bug

--- Assets/scripts/CarController.cs
+++ Assets/scripts/CarController.cs
@@ -49,10 +49,10 @@ public class CarController : MonoBehaviour {
 		turnTorque = turnTorque * Time.deltaTime * rb.mass;
 		rb.AddTorque (turnTorque);
 
-		if (transform.position.y <= 0.45f) {
-			Vector3 newPos = new Vector3(transform.position.x, 0.45f, transform.position.z);
-			rb.MovePosition (newPos);
-		}
+		// if (transform.position.y <= 0.45f) {
+		// 	Vector3 newPos = new Vector3(transform.position.x, 0.45f, transform.position.z);
+		// 	rb.MovePosition (newPos);
+		// }
 
 		//"Fake" rotate the car when you are turning
 		Vector3 newRotation = transform.eulerAngles;
