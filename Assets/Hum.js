d681c4d2c10a78c603e4b6e89aa431f7b340ba06
jackgorlin <jackgorlin@gmail.com>
2016-04-09 15:17:25 -0400

    jack slam

---
+++ Assets/Hum.js
@@ -0,0 +1,8 @@
+﻿// speed at which audio clip plays at its original pitch:
+var audioClipSpeed = 10.0;
+
+
+function Update() {
+    var p = GetComponent.<Rigidbody>().velocity.magnitude / audioClipSpeed;
+    GetComponent.<AudioSource>().pitch = Mathf.Clamp( p, 0.1, 4.0); // p is clamped to sane values
+}
