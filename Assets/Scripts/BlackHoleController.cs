93949463b75e9be1304733a4d08c7b701fac2501
grantaddis <grant@addis.net>
2016-04-11 22:15:38 +0100

    changed the rest of the files

--- Assets/Scripts/BlackHoleController.cs
+++ Assets/Scripts/BlackHoleController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class BlackHoleController : MonoBehaviour {
 
 	public float emissionStrength;
+	public float numOrbits;
 
 	void Start() {
 	}
@@ -15,7 +16,7 @@ public class BlackHoleController : MonoBehaviour {
 	IEnumerator WaitAndEmit(Collider other) {
 		CarController otherController = (CarController) other.gameObject.GetComponent(typeof(CarController));
 		otherController.EnterBlackHoleOrbit(transform.position);
-		yield return new WaitForSeconds(5.0f);
+		yield return new WaitForSeconds(numOrbits * 2.0f * Mathf.PI / otherController.blackHoleOrbitSpeed);
 		otherController.LeaveBlackHoleOrbit();
 		Rigidbody otherRB = other.attachedRigidbody;
 		otherRB.AddForce(other.transform.forward * emissionStrength);
