70846fc2fce52936c1c5994668f7f950c3945cca
Sam Giagtzoglou <sgiagtzoglou@wesleyan.edu>
2016-04-12 14:17:32 -0400

    configurator

---
+++ Assets/scripts/Configurator.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Configurator : MonoBehaviour {
+	public GameObject car;
+	public GameObject camera1p;
+	public GameObject camera12p;
+	public GameObject camera22p;
+	public GameObject camera13p;
+	public GameObject camera23p;
+	public GameObject camera33p;
+	public GameObject camera14p;
+	public GameObject camera24p;
+	public GameObject camera34p;
+	public GameObject camera44p;
+
+	void Start() {
+		if (SceneConfig.players == 0) {
+			SceneConfig.players = 1;
+		}
+		if (SceneConfig.players == 1) {
+			camera1p.gameObject.SetActive (true);
+
+			GameObject player = Instantiate(car);
+			TrackObject script = (TrackObject) camera1p.GetComponent("TrackObject");
+
+			script.target = player.transform;
+
+		} else if (SceneConfig.players == 2) {
+				
+			camera12p.gameObject.SetActive (true);
+			camera22p.gameObject.SetActive (true);
+
+			GameObject player1 = Instantiate(car);
+			GameObject player2 = Instantiate(car);
+
+			TrackObject script1 = (TrackObject) camera12p.GetComponent("TrackObject");
+			TrackObject script2 = (TrackObject) camera22p.GetComponent("TrackObject");
+
+			script1.target = player1.transform;
+			script2.target = player2.transform;
+
+		} else if (SceneConfig.players == 3) {
+			camera13p.gameObject.SetActive (true);
+			camera23p.gameObject.SetActive (true);
+			camera33p.gameObject.SetActive (true);
+
+			GameObject player1 = Instantiate(car);
+			GameObject player2 = Instantiate(car);
+			GameObject player3 = Instantiate(car);
+
+			TrackObject script1 = (TrackObject) camera13p.GetComponent("TrackObject");
+			TrackObject script2 = (TrackObject) camera23p.GetComponent("TrackObject");
+			TrackObject script3 = (TrackObject) camera33p.GetComponent("TrackObject");
+
+			script1.target = player1.transform;
+			script2.target = player2.transform;
+			script3.target = player3.transform;
+
+		} else if (SceneConfig.players == 4) {
+			camera14p.gameObject.SetActive (true);
+			camera24p.gameObject.SetActive (true);
+			camera34p.gameObject.SetActive (true);
+			camera44p.gameObject.SetActive (true);
+
+			GameObject player1 = Instantiate(car);
+			GameObject player2 = Instantiate(car);
+			GameObject player3 = Instantiate(car);
+			GameObject player4 = Instantiate(car);
+
+			TrackObject script1 = (TrackObject) camera14p.GetComponent("TrackObject");
+			TrackObject script2 = (TrackObject) camera24p.GetComponent("TrackObject");
+			TrackObject script3 = (TrackObject) camera34p.GetComponent("TrackObject");
+			TrackObject script4 = (TrackObject) camera44p.GetComponent("TrackObject");
+
+			script1.target = player1.transform;
+			script2.target = player2.transform;
+			script3.target = player3.transform;
+			script4.target = player4.transform;
+		}
+	}
+
+
+	void Awake()
+	{
+		DontDestroyOnLoad(this);
+	}
+
+
+		
+}
--- Assets/scripts/SceneConfig.cs
+++ Assets/scripts/SceneConfig.cs
@@ -5,94 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class SceneConfig : MonoBehaviour {
 	public GameObject car;
-	public Camera camera1p;
-	public Camera camera12p;
-	public Camera camera22p;
-	public Camera camera13p;
-	public Camera camera23p;
-	public Camera camera33p;
-	public Camera camera14p;
-	public Camera camera24p;
-	public Camera camera34p;
-	public Camera camera44p;
 	public static int score;
 
 	public static int players;
 
 	void Start() {
-		if (SceneManager.GetActiveScene().name == "game") {
-//			camera1p.enabled = false;
-//			camera12p.enabled = false;
-//			camera22p.enabled = false;
-//			camera13p.enabled = false;
-//			camera23p.enabled = false;
-//			camera33p.enabled = false;
-//			camera14p.enabled = false;
-//			camera24p.enabled = false;
-//			camera34p.enabled = false;
-//			camera44p.enabled = false;
-			if (players == 1) {
-				camera1p.enabled = true;
-				
-				Camera cam = (Camera) camera1p.GetComponent("Camera");
-				cam.tag = "MainCamera";
-				cam.enabled = true ;
-				GameObject player = Instantiate(car);
-				TrackObject script = (TrackObject) camera1p.GetComponent("TrackObject");
 
-				script.target = player.transform;
-
-			} else if (players == 2) {
-				
-				GameObject player1 = Instantiate(car);
-				GameObject player2 = Instantiate(car);
-
-				TrackObject script1 = (TrackObject) camera12p.GetComponent("TrackObject");
-				TrackObject script2 = (TrackObject) camera22p.GetComponent("TrackObject");
-
-				script1.target = player1.transform;
-				script2.target = player2.transform;
-
-			} else if (players == 3) {
-				GameObject player1 = Instantiate(car);
-				GameObject player2 = Instantiate(car);
-				GameObject player3 = Instantiate(car);
-
-				TrackObject script1 = (TrackObject) camera13p.GetComponent("TrackObject");
-				TrackObject script2 = (TrackObject) camera23p.GetComponent("TrackObject");
-				TrackObject script3 = (TrackObject) camera33p.GetComponent("TrackObject");
-
-				script1.target = player1.transform;
-				script2.target = player2.transform;
-				script3.target = player3.transform;
-
-			} else if (players == 4) {
-				GameObject player1 = Instantiate(car);
-				GameObject player2 = Instantiate(car);
-				GameObject player3 = Instantiate(car);
-				GameObject player4 = Instantiate(car);
-
-				TrackObject script1 = (TrackObject) camera14p.GetComponent("TrackObject");
-				TrackObject script2 = (TrackObject) camera24p.GetComponent("TrackObject");
-				TrackObject script3 = (TrackObject) camera34p.GetComponent("TrackObject");
-				TrackObject script4 = (TrackObject) camera44p.GetComponent("TrackObject");
-
-				script1.target = player1.transform;
-				script2.target = player2.transform;
-				script3.target = player3.transform;
-				script4.target = player4.transform;
-			}
-//			Camera player1camera = Instantiate(topCamera2p);
-
-//			Component com = GetComponent ("TrackObject");
-//			com.pa
-//			player1camera.gameObject.AddComponent (com);
-			for (int x = 0; x < players; x++) {
-//				GameObject player = Instantiate(car);
-//				cube.AddComponent<Rigidbody>();
-//				cube.transform.position = new Vector3(x, y, 0);
-			}
-		}
 	}
 
 	void Awake()
@@ -101,7 +19,9 @@ public class SceneConfig : MonoBehaviour {
 	}
 
 	public void LoadLevel(string level){
+		Debug.Log (players);
 		Application.LoadLevel(level);
+
 
 	}
 
