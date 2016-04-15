4e748d75bd49b0ac87bc4365dd702b06ccb8f4b9
fru1tpunch <dhyeon@wesleyan.edu>
2016-04-15 12:40:24 -0400

    final edits
    hopefully everything is lined up now

---
+++ Assets/motionblurtrigger.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.ImageEffects;
+
+public class motionblurtrigger: MonoBehaviour{
+	
+	Camera camera;
+	MotionBlurFilter motionblur;
+	void Start () 
+	{
+		motionblur = (MotionBlurFilter) gameObject.GetComponent(Camera<MotionBlurFilter>);
+		motionblur = false;
+
+	
+	}
+
+void OnTriggerEnter()
+{
+
+		motionblur = true;
+	
+}
+
+void OnTriggerExit ()
+{
+	motionblur = false;
+
+	}
+}
+
