73f75e26f5ffcf94b771fbbc5100fef698993732
jackgorlin <jackgorlin@gmail.com>
2016-04-19 15:02:01 -0400

    packages

--- Assets/Standard Assets/Effects/ImageEffects/Scripts/ImageEffectBase.cs
+++ Assets/Standard Assets/Effects/ImageEffects/Scripts/ImageEffectBase.cs
@@ -1,3 +1,13 @@
+/*
+http://www.cgsoso.com/forum-257-1.html
+
+CG搜搜 Unity3d 插件团购
+
+CGSOSO 主打游戏开发，影视设计等CG资源素材。
+
+每日Unity3d插件免费更新，仅限下载试用，如若商用，请务必官网购买！
+*/
+
 using UnityEngine;
 
 [RequireComponent (typeof(Camera))]
@@ -8,7 +18,7 @@ public class ImageEffectBase : MonoBehaviour {
 	public Shader   shader;
 	private Material m_Material;
 
-	protected void Start ()
+	protected virtual void Start ()
 	{
 		// Disable if we don't support image effects
 		if (!SystemInfo.supportsImageEffects) {
@@ -32,7 +42,7 @@ public class ImageEffectBase : MonoBehaviour {
 		} 
 	}
 	
-	protected void OnDisable() {
+	protected virtual void OnDisable() {
 		if( m_Material ) {
 			DestroyImmediate( m_Material );
 		}
