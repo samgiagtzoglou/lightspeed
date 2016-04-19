4981b9de0b28e6867d70397af24d4de58487b865
jackgorlin <jackgorlin@gmail.com>
2016-04-18 23:58:47 -0400

    Menu Animations

---
+++ Assets/ButtonIncrease.js
@@ -0,0 +1,14 @@
+﻿ #pragma strict
+ 
+ private var smallerRect = Rect(20,20,100,50);
+ private var biggerRect = Rect(15,15,110,60);
+ 
+ function OnGUI() {
+     var rect = smallerRect;
+     if (smallerRect.Contains(Event.current.mousePosition))
+         rect = biggerRect;
+         
+     if (GUI.Button(rect, "Some Text")) {
+         Debug.Log("Button pressed");
+     }
+ }
