cc0189fab20e58702e44364555f34af54ab70414
jackgorlin <jackgorlin@gmail.com>
2016-04-24 11:12:40 -0400

    More but don't pull

--- Assets/RightArrowTrigger.js
+++ Assets/RightArrowTrigger.js
@@ -76,11 +76,11 @@ function OnGUI()
       // GUI.DrawTexture(Rect(Screen.width - 480, Screen.height -525, 280, 145), riddle);
        //GUI.DrawTexture(Rect(Screen.width /2, 0, 280, 145), riddle);
        //GUI.DrawTexture(Rect(Screen.width/4, 0, (Screen.width/2), Screen.height/5), riddle, ScaleMode.ScaleToFit); FOR NOT BARS
-        GUI.DrawTexture(Rect(Screen.width/4, 0, (Screen.width/2), Screen.height/17), riddle, ScaleMode.ScaleToFit);
-        GUI.DrawTexture(Rect(Screen.width/2, 0, (Screen.width/2), Screen.height/17), riddle1, ScaleMode.ScaleToFit);
-         GUI.DrawTexture(Rect(0, 0, (Screen.width/2), Screen.height/17), riddle2, ScaleMode.ScaleToFit);
-         GUI.DrawTexture(Rect(Screen.width/1.4, 0, (Screen.width/2), Screen.height/17), riddle3, ScaleMode.ScaleToFit);
-         GUI.DrawTexture(Rect(0-Screen.width/4.6, 0, (Screen.width/2), Screen.height/17), riddle4, ScaleMode.ScaleToFit);
+        GUI.DrawTexture(Rect(Screen.width/4, 0, (Screen.width/2), Screen.height/8), riddle, ScaleMode.ScaleToFit);
+        GUI.DrawTexture(Rect(Screen.width/2, 0, (Screen.width/2), Screen.height/8), riddle1, ScaleMode.ScaleToFit);
+                      GUI.DrawTexture(Rect(0, 0, (Screen.width/2), Screen.height/8), riddle2, ScaleMode.ScaleToFit);
+         GUI.DrawTexture(Rect(Screen.width/1.44, 0, (Screen.width/2), Screen.height/8), riddle3, ScaleMode.ScaleToFit);
+         GUI.DrawTexture(Rect(0-Screen.width/5, 0, (Screen.width/2), Screen.height/8), riddle4, ScaleMode.ScaleToFit);
 
         }
 
--- Assets/RoidsTrigger.js
+++ Assets/RoidsTrigger.js
@@ -3,6 +3,8 @@
 private var guiShow : boolean = false;
 
 var riddle : Texture;
+var riddle1 : Texture;
+var riddle2 : Texture;
 
 function OnTriggerStay (Col : Collider)
 
@@ -30,6 +32,8 @@ function OnGUI()
 	{
 		//GUI.DrawTexture(Rect(Screen.width - 480, Screen.height -725, 480, 145), riddle);
 			//	GUI.DrawTexture(Rect(Screen.width /4, Screen.height /4.5, 480, 145), riddle);
+				GUI.DrawTexture(Rect(Screen.width /2, -5, (Screen.width/1.3), Screen.height/14), riddle1, ScaleMode.ScaleToFit);
+							  GUI.DrawTexture(Rect(0- Screen.width/8, -5, (Screen.width/1.3), Screen.height/14), riddle2, ScaleMode.ScaleToFit);
 			GUI.DrawTexture(Rect(Screen.width /4, 0, (Screen.width/2), Screen.height/16), riddle, ScaleMode.ScaleToFit);
 
 	}
