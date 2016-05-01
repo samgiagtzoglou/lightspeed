414fb1d7c4a3f1d459fc5e41aa5c6ce966db9909
Sam Giagtzoglou <sgiagtzoglou@wesleyan.edu>
2016-05-01 13:59:56 -0400

    pause screen

--- Assets/scripts/PositionAndLapManager.cs
+++ Assets/scripts/PositionAndLapManager.cs
@@ -11,7 +11,7 @@ public class PositionAndLapManager : MonoBehaviour {
 	public Dictionary<string,int> cartPositions;
 	public int numLaps = 1;
 	public Text timerText;
-
+	public bool isPause = false;
 	public void Start() {
 		numPlayers = SceneConfig.players;
 		// set up the car objects
@@ -33,6 +33,20 @@ public class PositionAndLapManager : MonoBehaviour {
 
 	// this gets called every frame
 	public void Update() {
+		
+		if( Input.GetButtonDown("C1_Start") || Input.GetButtonDown("C2_Start") || 
+			Input.GetButtonDown("C3_Start") || Input.GetButtonDown("C4_Start") ||
+			Input.GetButtonDown("Pause")){
+			isPause = !isPause;
+				if(isPause){
+					GameObject.Find ("raceManager").transform.FindChild ("pauseText").gameObject.SetActive(true);
+					Time.timeScale = 0;
+				} else {
+					GameObject.Find ("raceManager").transform.FindChild ("pauseText").gameObject.SetActive(false);
+					Time.timeScale = 1;
+				}
+		}
+	
 		foreach (CartPosition pos in allCarts) {
 			Debug.Log (pos);
 			Debug.Log (pos.lastWaypoint);
