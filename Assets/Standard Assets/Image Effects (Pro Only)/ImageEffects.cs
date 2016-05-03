ef4337e043f56da8673127f0ad0d325db6a74379
Sam Giagtzoglou <sgiagtzoglou@wesleyan.edu>
2016-05-03 02:50:05 -0400

    more fixes

--- Assets/Standard Assets/Image Effects (Pro Only)/ImageEffects.cs
+++ Assets/Standard Assets/Image Effects (Pro Only)/ImageEffects.cs
@@ -1,47 +1,42 @@
-/*
-http://www.cgsoso.com/forum-257-1.html
-
-CG搜搜 Unity3d 插件团购
-
-CGSOSO 主打游戏开发，影视设计等CG资源素材。
-
-每日Unity3d插件免费更新，仅限下载试用，如若商用，请务必官网购买！
-*/
-
+using System;
 using UnityEngine;
-//using System;
 
-/// A Utility class for performing various image based rendering tasks.
-[AddComponentMenu("")]
-public class ImageEffects
+namespace UnityStandardAssets.ImageEffects
 {
-    public static void RenderDistortion(Material material, RenderTexture source, RenderTexture destination, float angle, Vector2 center, Vector2 radius)
+    /// A Utility class for performing various image based rendering tasks.
+    [AddComponentMenu("")]
+    public class ImageEffects
     {
-        bool invertY = source.texelSize.y < 0.0f;
-        if (invertY)
+        public static void RenderDistortion(Material material, RenderTexture source, RenderTexture destination, float angle, Vector2 center, Vector2 radius)
         {
-            center.y = 1.0f - center.y;
-            angle = -angle;
+            bool invertY = source.texelSize.y < 0.0f;
+            if (invertY)
+            {
+                center.y = 1.0f - center.y;
+                angle = -angle;
+            }
+
+            Matrix4x4 rotationMatrix = Matrix4x4.TRS(Vector3.zero, Quaternion.Euler(0, 0, angle), Vector3.one);
+
+            material.SetMatrix("_RotationMatrix", rotationMatrix);
+            material.SetVector("_CenterRadius", new Vector4(center.x, center.y, radius.x, radius.y));
+            material.SetFloat("_Angle", angle*Mathf.Deg2Rad);
+
+            Graphics.Blit(source, destination, material);
         }
 
-        Matrix4x4 rotationMatrix = Matrix4x4.TRS(Vector3.zero, Quaternion.Euler(0, 0, angle), Vector3.one);
 
-        material.SetMatrix("_RotationMatrix", rotationMatrix);
-        material.SetVector("_CenterRadius", new Vector4(center.x, center.y, radius.x, radius.y));
-        material.SetFloat("_Angle", angle * Mathf.Deg2Rad);
+        [Obsolete("Use Graphics.Blit(source,dest) instead")]
+        public static void Blit(RenderTexture source, RenderTexture dest)
+        {
+            Graphics.Blit(source, dest);
+        }
 
-        Graphics.Blit(source, destination, material);
+
+        [Obsolete("Use Graphics.Blit(source, destination, material) instead")]
+        public static void BlitWithMaterial(Material material, RenderTexture source, RenderTexture dest)
+        {
+            Graphics.Blit(source, dest, material);
+        }
     }
-    
-	[System.Obsolete("Use Graphics.Blit(source,dest) instead")]
-	public static void Blit(RenderTexture source, RenderTexture dest)
-	{
-		Graphics.Blit(source, dest);
-	}
-
-	[System.Obsolete("Use Graphics.Blit(source, destination, material) instead")]
-	public static void BlitWithMaterial(Material material, RenderTexture source, RenderTexture dest)
-	{
-		Graphics.Blit(source, dest, material);
-	}        
 }
